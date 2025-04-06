78db6231229cb6f47a3bde6e82c24957f0f8055e
ramakrishna-admin <ramakrishnakannekanti.in@outlook.com>
2025-04-06 14:50:32 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "kittubucket" 
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
